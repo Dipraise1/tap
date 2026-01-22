4d756a8648eee8c9ff35b70c0b28d3b594c5017a
dipraise1 <raphealdivine2@gmail.com>
2026-01-22 18:20:26 +0100

    Restore dependencies and fix styles import

--- components/WalletProvider.tsx
+++ components/WalletProvider.tsx
@@ -10,7 +10,7 @@ import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adap
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 
-require("@solana/wallet-adapter-react-ui/styles.css");
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 export const WalletProvider: FC<{ children: ReactNode }> = ({ children }) => {
   // Can be set to 'devnet', 'testnet', or 'mainnet-beta'.
